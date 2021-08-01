dbf163d5b81f71fd7036cfd92b933c1a1ae22905
Clare Wei <c53wei@edu.uwaterloo.ca>
2021-08-01 07:44:26 -0400

    Rewrite after reading documentation

--- element_tree.py
+++ element_tree.py
@@ -1,26 +1,25 @@
 import xml.etree.ElementTree as ET
 
+from collections import defaultdict
+
 tree = ET.parse('jan.vsk')
 root = tree.getroot()
 
-# <Parameter> contains values corresponding to marker locations
-parameters = root[0]
+# <Parameter> section contains xyz components of marker information
 marker_xyz = {}
-for x in parameters:
-    if x.tag == 'Parameter':
-        marker_xyz[x.attrib['NAME']] = x.attrib['VALUE']
+for marker_coord in root.findall('.//Parameter'):
+        marker_xyz[marker_coord.get('NAME')] = marker_coord.get('VALUE')
 
-# Skeleton contains segment and corresponding joint in tree format
-skeleton = root[1]
-segment_root = skeleton[0]
-# Target set contains objects that reference location of markers in <Parameters>
-target_set = root[3][0]
-markers = {}
-for x in target_set:
-    location_xyz_names = str.split(x.attrib['POSITION'], ' ')
+# <TargetLocalPointToWorldPoint> contains segment and marker data
+segment_marker_data = defaultdict(lambda: defaultdict(dict))
+for marker in root.findall('.//TargetLocalPointToWorldPoint'):
+    segment_name = marker.get('SEGMENT')
+    marker_name = marker.get('MARKER')
+
+    location_xyz_names = str.split(marker.get('POSITION'), ' ')
     location_xyz_values = [marker_xyz.get(location_name.strip('\''), location_name) \
                            for location_name in location_xyz_names]
-    markers[x.attrib['MARKER']] = location_xyz_values
+    segment_marker_data[segment_name][marker_name] = location_xyz_values
 
 
 
