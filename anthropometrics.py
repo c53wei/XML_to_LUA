8bb7a7a0efaf6201249a49346d07424c370d840e
Clare Wei <c53wei@edu.uwaterloo.ca>
2021-08-01 09:33:59 -0400

    Stub fields for mass, COM, and inertia

---
+++ anthropometrics.py
@@ -0,0 +1,4 @@
+def calc_anthro():
+    "Stub function to return mass, com, and inertial"
+    # ToDo: Use database for calcs
+    return [0, [0, 0, 0], [[0, 0, 0]]*3]
--- tree_attributes.py
+++ tree_attributes.py
@@ -1,6 +1,8 @@
 import numpy as np
 import xml.etree.ElementTree as ET
 
+from anthropometrics import calc_anthro
+
 
 def add_parent_info(et: ET.Element):
     """
@@ -8,19 +10,19 @@ def add_parent_info(et: ET.Element):
     :param et: Starting point of tree, can be root or any node
     """
     for child in et:
-        child.attrib['__parent__'] = et
+        child.attrib['parent'] = et
         add_parent_info(child)
 
 
 def strip_parent_info(et: ET.Element):
     for child in et:
-        child.attrib.pop('__parent__', 'None')
+        child.attrib.pop('parent', 'None')
         strip_parent_info(child)
 
 
 def get_parent(et: ET.Element):
     if '__parent__' in et.attrib:
-        return et.attrib['__parent__']
+        return et.attrib['parent']
     else:
         return None
 
@@ -32,25 +34,31 @@ def add_position_info(root: ET.Element, segment_data: {}):
     :param segment_data: Nested dictionary in form {segment_name: {marker_name: [x, y, z]...}
     """
     for child in root.findall('.//Segment'):
-        child.attrib['__markers__'] = segment_data[child.get('NAME')]
-        child.attrib['__r__'] = child[0].get('PRE-POSITION')
-        child.attrib['__E__'] = np.identity(3)  # Stub until rotation matrix is verified
+        # Anthropometric info, stub
+        child.attrib['body'] = dict(zip(['mass', 'com', 'inertia'], calc_anthro()))
 
+        # Position info
+        child.attrib['joint_frame'] = {}
+        child.attrib['joint_frame']['r'] = child[0].get('PRE-POSITION')
+        child.attrib['joint_frame']['E'] = np.identity(3)  # ToDo: Stub until rotation matrix is verified
+
+        # Marker info
+        child.attrib['markers'] = segment_data[child.get('NAME')]
         add_position_info(child, segment_data)
         # print(f'{child.get("NAME")}\t{child.get("__r__")}\t{child.get("__markers__")}')
 
 
 def get_markers(et):
     "Gets markers and their positions of a segment"
-    if '__markers__' in et.attrib:
-        return et.attrib['__markers__']
+    if 'markers' in et.attrib:
+        return et.attrib['markers']
     else:
         return None
 
 
 def get_r(et):
     "Gets position relative to parent segment"
-    if '__r__' in et.attrib:
-        return et.attrib['__r__']
+    if 'r' in et.attrib['joint_frame']:
+        return et.attrib['joint_frame']['r']
     else:
         return None
